c7a4b04b1d8836f8ea9cbe168727516cdae62dbb
Leon Berenschot <leonb@beriedata.nl>
2010-03-25 13:33:22 +0100

    use the currency formatter of Rails

--- lib/acts_as_currency.rb
+++ lib/acts_as_currency.rb
@@ -1,15 +1,13 @@
 class ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper # for currency formatting
 
-	def self.acts_as_currency(*args)
+  def self.acts_as_currency(*args)
 
-		args.each do |arg|
-		
-			define_method arg.to_s + '=' do |currency_string|
-				currency_string.gsub!(/^\$/, '') if currency_string =~ /^\$/
-				write_attribute(arg, currency_string)
-			end
-		
-		end
+    args.each do |meth|
+      define_method "formatted_#{meth}" do
+        number_to_currency(self.send(meth))
+      end
+    end
 
 		define_method 'validate' do
 			args.each { |arg| errors.add(arg, "should be at least 0.01") if eval(arg.to_s).nil? || eval(arg.to_s) < 0.01 }
